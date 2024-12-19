e8ec0b937e9373e8b1c945983965bcef60b90fe4
Natanael Felix <natanaelximenes@gmail.com>
2024-12-19 11:09:54 -0300

    feat: created schema

---
+++ lib/xhist/gists/gist.ex
@@ -0,0 +1,23 @@
+defmodule Xhist.Gists.Gist do
+  use Ecto.Schema
+  import Ecto.Changeset
+
+  @primary_key {:id, :binary_id, autogenerate: true}
+  @foreign_key_type :binary_id
+  schema "gists" do
+    field :name, :string
+    field :description, :string
+    field :markup_text, :string
+    belongs_to :user, Xhist.Accounts.User
+    has_many :comments, Xhist.Comments.Comment
+
+    timestamps(type: :utc_datetime)
+  end
+
+  @doc false
+  def changeset(gist, attrs) do
+    gist
+    |> cast(attrs, [:name, :description, :markup_text])
+    |> validate_required([:name, :description, :markup_text])
+  end
+end
